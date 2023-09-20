0b93f5f3d6d0bdeffb8c56d9b8f148cc1875a7c8
janosiccardi <janosiccardi99@gmail.com>
2023-09-20 11:53:32 -0300

    Exclude auto configuration to data source

--- src/main/java/com/juanpablochiesa/backend/BackendApplication.java
+++ src/main/java/com/juanpablochiesa/backend/BackendApplication.java
@@ -2,8 +2,12 @@ package com.juanpablochiesa.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
